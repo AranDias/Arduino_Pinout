015eccaab06aa0043218b81de6be4a4f07612195
AranDias <a248116@dac.unicamp.br>
2022-11-04 16:07:10 -0300

    04/11/2022

--- app/src/main/java/com/example/arduinopinout/MainActivity.java
+++ app/src/main/java/com/example/arduinopinout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.arduinopinout;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,11 +18,13 @@ import com.example.arduinopinout.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private Button ESP38;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +33,16 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        /*setSupportActionBar(binding.toolbar);*/
 
-/*        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        ESP38 = findViewById(R.id.ButtonESP38PIN);
+        ESP38.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent ESP38Intent = new Intent("ESP39");
+                startActivity(ESP38Intent);
             }
-        });*/
+        });
     }
 
     @Override
@@ -65,11 +66,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-/*    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }*/
 }
